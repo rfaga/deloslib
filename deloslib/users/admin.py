442724302757b44808ed10b54a25fd98aa9952ac
Roberto Faga <faga@caule.net>
2012-08-10 19:35:28 -0300

    Changing admin

--- deloslib/users/admin.py
+++ deloslib/users/admin.py
@@ -4,6 +4,39 @@
 from django.contrib import admin
 from models import *
 
+from django import forms
+from admin_enhancer import admin as enhanced_admin
+
+
 admin.site.register(Unidade, admin.ModelAdmin)
-admin.site.register(Person, admin.ModelAdmin)
-admin.site.register(Role, admin.ModelAdmin)
+
+class EnhancedModelAdmin(enhanced_admin.EnhancedModelAdminMixin,
+                         admin.ModelAdmin):
+    pass
+
+class PersonForm(forms.ModelForm):
+    roles = forms.ModelMultipleChoiceField(queryset=Role.objects.order_by('word'),)
+    class Meta:
+        model = Person
+
+class RoleInline(admin.TabularInline):
+    model = Role
+    extra = 1
+    
+
+class PersonAdmin(EnhancedModelAdmin):
+    inlines = (RoleInline,)
+    readonly_fields = ('email', 'is_superuser')
+    def email(self, obj):
+        return obj.user.email
+    def is_superuser(self, obj):
+        if obj.user.is_superuser:
+            return u"Sim"
+        else:
+            return u"Não" 
+    
+    is_superuser.short_description = 'Super Administrador?'
+    
+    
+admin.site.register(Person, PersonAdmin)
+#admin.site.register(Role, admin.ModelAdmin)
--- deloslib/users/models.py
+++ deloslib/users/models.py
@@ -21,31 +21,47 @@ class Person(models.Model): #UserProfile
     nro_usp = models.CharField(_(u'Numero USP'), max_length=10, blank=True, null=True)
     name = models.CharField(_(u'Nome'), max_length=150, blank=True, null=True)
     user = models.ForeignKey(User, unique=True, blank=True, null=True)
-    
+    class Meta:
+        verbose_name = _(u"Pessoa")
+        
+        
     def __unicode__(self):
         return u'%s' % (self.name)
     
     def get_role(self, app_id):
         try:
-            return Role.objects.get(unidade=self.unidade, app_id=app_id, user=self.user)
+            return Role.objects.get(unidade=self.unidade, app_id=app_id, person=self)
         except:
             return Role(role='')
 
 
 RELATIONS = (
-    ('', 'None'),
     ('A', _(u'Administrador')),
     ('C', _(u'Convidado')),
-    ('O', _(u'Operador')),
+    ('O', _(u'Operador / Técnico')),
+    ('D', _(u'Docente')),
+    
+)
+APP_IDS = (
+    ('users', _(u'Módulo de Usuários')),
+    ('news', _(u'Módulo de Notícias')),
+    ('nfe', _(u'Módulo de Notas Fiscais')),
+    ('gradprogram', _(u'Módulo de Inscrição na Pós')),
+    ('conference', _(u'Módulo de Conferências')),
+    ('memo', _(u'Módulo de Memorandos')),
 )
 class Role(models.Model):
-    user = models.ForeignKey(User)
-    app_id = models.CharField(max_length=30, verbose_name=_(u'ID da aplicação'))
+    person = models.ForeignKey(Person)
+    app_id = models.CharField(max_length=30, verbose_name=_(u'ID da aplicação'), choices=APP_IDS)
     unidade = models.ForeignKey(Unidade, verbose_name=_(u'Unidade atual'))
     role = models.CharField(max_length=1, choices=RELATIONS)
+    
+    class Meta:
+        verbose_name = _(u"Regra ou Permissão")
+        verbose_name_plural = _(u"Regras e Permissões")
     
     def is_admin(self):
         return self.role == 'A'
 
     def __unicode__(self):
-        return u'%s (%s) - %s' % (self.get_role_display(), self.app_id, self.user)
+        return u'%s (%s) - %s' % (self.get_role_display(), self.app_id, self.person)
