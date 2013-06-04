ac8ce3ce7f866321f7c83f88e81a0a2dd08dd487
Roberto Faga <robertofaga@gmail.com>
2013-06-03 21:28:36 -0300

    Better forms

--- deloslib/users/static/delos.js
+++ deloslib/users/static/delos.js
@@ -3,9 +3,9 @@ $(function() {
 	//$('.control-group input').parents('.control-group').addClass('span6');
 	//$('.control-group').addClass('span12');
 	
-	$("textarea, input, select").on('focus', function(){
+/*	$("textarea, input, select").on('focus', function(){
 		$(this).parents('.controls').find(".help-block").css('display', 'inline'); });
 	$("textarea, input, select").on('blur', function(){ 
 		$(this).parents('.controls').find(".help-block").css('display', 'none'); });
-	
+	*/
 });
